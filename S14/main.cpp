eb9a4e21262ea75b6d04b8d0856d50f40386453d
alonso804 <alonsobs804@gmail.com>
2022-11-17 19:04:11 -0500

    S14

--- S14/main.cpp
+++ S14/main.cpp
@@ -1,10 +1,54 @@
 #include <iostream>
+#include <vector>
 #include <fstream>
 
 #define deb(x) cout << #x << ": " << x << endl;
 #define FOR(i,a,b) for (int i = (a); i < (b); i++)
 
 using namespace std;
+
+void leer();
+void escribir();
+
+class Persona;
+
+void filePersona(const string&, Persona&);
+void filePersona(const string&, vector<Persona*>&);
+
+ostream& operator << (ostream&, const Persona&);
+ostream& operator << (ostream&, const vector<Persona*>&);
+
+class Persona {
+  string nombre;
+  int edad;
+
+public:
+  Persona(string nombre, int edad) {
+    this->nombre = nombre;
+    this->edad = edad;
+  }
+
+  friend ostream& operator << (ostream&, const Persona&);
+  friend ostream& operator << (ostream&, const vector<Persona*>&);
+
+  ~Persona() {}
+};
+
+
+int main() {
+  vector<Persona*> personas;
+  personas.push_back(new Persona("Juan", 20));
+  personas.push_back(new Persona("Pedro", 30));
+  personas.push_back(new Persona("Maria", 40));
+
+  filePersona("personas.csv", personas);
+
+  for (auto& persona : personas) {
+    delete persona;
+  }
+
+  return 0;
+}
 
 void leer() {
   // ifstream file("temp.txt");
@@ -30,8 +74,38 @@ void escribir() {
   file.close();
 }
 
-int main() {
-  escribir();
+void filePersona(const string& filename, Persona& persona) {
+  fstream file(filename, ios::out);
 
-  return 0;
+  // file << "Nombre: " << persona.nombre << endl;
+  // file << "Edad: " << persona.edad << endl;
+
+  file << persona << endl;
+
+  file.close();
+}
+
+ostream& operator << (ostream& os, const Persona& persona) {
+  os << "nombre,edad" << endl;
+  os << persona.nombre << "," << persona.edad;
+
+  return os;
+}
+
+void filePersona(const string& filename, vector<Persona*>& personas) {
+  fstream file(filename, ios::out);
+
+  file << personas << endl;
+
+  file.close();
+}
+
+ostream& operator << (ostream& os, const vector<Persona*>& personas) {
+  os << "nombre,edad" << endl;
+
+  for (const auto& persona : personas) {
+    os << persona->nombre << "," << persona->edad << endl;
+  }
+
+  return os;
 }
